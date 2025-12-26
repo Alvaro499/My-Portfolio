54d06fe3aa560b4a8c07caaae9a76f3d90b11ad0
Alvaro499 <alvarosiles499@gmail.com>
2025-12-25 19:48:36 -0600

    Education-Section Page: Finished

--- src/pages/education-section.jsx
+++ src/pages/education-section.jsx
@@ -10,37 +10,70 @@ export default function EducationSection() {
 
         <>
             <main className={styles.container}>
+                <h2>Education</h2>
 
                 <section className={styles.cardContainer}>
-                    <h2>Education</h2>
-                    <div style={styles.card}>
 
-                        <div cardTitleContainer>
-                            <h2 className={styles.cardTitle}>Bachelor of Science in Computer Science</h2>
-                            <ul style={styles.cardTitleEquivalents}>
-                                <li>Bachelor of Science </li>
-                                <li>Bachelor of Science </li>
-                                <li>Bachelor of Science </li>
+                    {/* CARD 1 */}
+                    <div className={styles.card}>
+
+                        <div className={styles.cardHeader}>
+                            <h4 className={styles.cardTitle}>
+                                Bachelor of Science in Computer Science
+                            </h4>
+
+                            <ul className={styles.cardTitleEquivalents}>
+                                <li>Bachelor of Science</li>
+                                <li>Bachelor of Science</li>
                             </ul>
                         </div>
-                        <p className={styles.cardInstitution}>University of Technology</p>
-                        <p className={styles.cardDuration}>2015 - 2019</p>
-                        <span>In Progress</span>
+
+                        <div className={styles.cardBody}>
+                            <p className={styles.cardInstitution}>
+                                University of Technology
+                            </p>
+                            <p className={styles.cardDuration}>
+                                2015 - 2019
+                            </p>
+                        </div>
+
+                        <div className={styles.cardFooter}>
+                            <span>In Progress</span>
+                        </div>
+
                     </div>
 
-                    <div style={styles.card}>
+                    {/* CARD 2 */}
+                    <div className={styles.card}>
 
-                        <div cardTitleContainer>
-                            <h2 className={styles.cardTitle}>Technician in Design and Development Web</h2>
-                            <ul style={styles.cardTitleEquivalents}>
-                                
+                        <div className={styles.cardHeader}>
+                            <h4 className={styles.cardTitle}>
+                                Technician in Design and Development Web
+                            </h4>
+
+                            <ul className={styles.cardTitleEquivalents}>
                             </ul>
                         </div>
-                        <p className={styles.cardInstitution}>Instituto Nacional de Aprendizaje (INA)</p>
-                        <p className={styles.cardDuration}>201 - 2019</p>
-                        <span>In Progress</span>
+
+                        <div className={styles.cardBody}>
+                            <p className={styles.cardInstitution}>
+                                Instituto Nacional de Aprendizaje (INA)
+                            </p>
+                            <p className={styles.cardDuration}>
+                                201 - 2019
+                            </p>
+                        </div>
+
+                        <div className={styles.cardFooter}>
+                            <span>In Progress</span>
+                        </div>
+
                     </div>
+
+                    
+
                 </section>
+
             </main>
 
         </>
