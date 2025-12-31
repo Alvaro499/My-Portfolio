1d5a8ce5e71150cb9ce9218f823ec5653e8e4d5f
Alvaro499 <alvarosiles499@gmail.com>
2025-12-31 14:16:06 -0600

    Solution of Errors with Icons from Technologies

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,22 +1,41 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import styles from "./navbar.module.css"
 
-import { Menu, User, Phone, Briefcase, Folder, GraduationCap, Cpu} from 'lucide-react';
+import { Menu, User, Phone, Briefcase, Folder, GraduationCap, Cpu } from 'lucide-react';
 
 export default function Navbar() {
     const [menuState, setMenuState] = useState(false);
+     const navbarRef = useRef(null);
+     
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (
+                menuState &&
+                navbarRef.current &&
+                !navbarRef.current.contains(event.target)
+            ) {
+                setMenuState(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [menuState]);
 
     return (
         <>
-            <header className={styles.headerContainer}>
+            <header className={styles.headerContainer} ref={navbarRef}>
 
                 <nav className={styles.globalNavbar}>
-    
+
                     <ul className={`${styles.navList} ${menuState ? styles.navListActive : ""}`}>
                         <li className={styles.menuLink} onClick={() => setMenuState(!menuState)}>
-                            <Link href=""><Menu /><span>Menu</span></Link>
+                            <Link href="#me"><Menu /><span>Menu</span></Link>
                         </li>
 
                         <li>
@@ -26,10 +45,10 @@ export default function Navbar() {
                             <Link href="/#degrees"><GraduationCap /><span>Degrees</span></Link>
                         </li>
                         <li>
-                            <Link href="/#tech"><Cpu/><span>Technologies</span></Link>
+                            <Link href="/#tech"><Cpu /><span>Technologies</span></Link>
                         </li>
                         <li>
-                            <Link href="/#projects"><Briefcase/><span>Projects</span></Link>
+                            <Link href="/#projects"><Briefcase /><span>Projects</span></Link>
                         </li>
                         <li>
                             <Link href="/#contact"><Phone /><span>Contact</span></Link>
